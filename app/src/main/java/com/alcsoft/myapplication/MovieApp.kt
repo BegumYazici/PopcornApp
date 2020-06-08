9dc7ea0767db20dcf43693ca0563e8c14c2918ba
Begum Yazici <yazicibegum@gmail.com>
2020-06-08 15:16:22 +0100

    Add LeakCanary in MoviesApp

---
+++ app/src/main/java/com/alcsoft/myapplication/MovieApp.kt
@@ -0,0 +1,10 @@
+package com.alcsoft.myapplication
+
+import androidx.multidex.MultiDexApplication
+
+class MovieApp : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
